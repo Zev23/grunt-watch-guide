56351ab9ecd60003686887cb31fe74d3a88095ea
Zev <zev.teh@gmail.com>
2014-03-11 13:33:26 +0800

    Changed from localhost to dynamic server host

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   
   //For LiveReload
-  res.write("<script src=\"http://localhost:35729/livereload.js\"></script>");
+  res.write("<script src=\"http://"+req.headers.host.split(":")[0]+":35729/livereload.js\"></script>");
 
   res.end('Hello World\n');
 }).listen(3000);
